76fe50ca6e343085110ad90fcacc463725882fa8
MYCOMPUTER\kyahhokid <kyahhokid@me.com>
2019-01-15 22:34:11 +0900

    設定リポジトリを作成 #3

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,79 @@ void main() {
     DeviceOrientation.portraitDown
   ]);
 
-  runApp(
-      ScopedModel<CuccoGameMaster>(
-        model: CuccoGameMaster(),
-        child: CuccoApp()
-      )
-  );
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Startup NameGenerator'),
+            ),
+            body: MainBody()
+        ),
+    );
+  }
+}
+
+class MainBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainBodyState();
+}
+
+class MainBodyState extends State<MainBody> {
+
+  String _value = "1";
+  List<DropdownMenuItem<String>> _items;
+
+  @override
+  Widget build(BuildContext context) {
+    _items = createItems();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        ListTile(
+            title: Text("NPCの人数"),
+            trailing: DropdownButton<String>(value: _value, items: _items, onChanged: ((String newValue) {
+              setState(() {
+                _value = newValue;
+              });
+            })), 
+        ),
+        RaisedButton(
+          child: Text("ゲームスタート"),
+          onPressed: _transitionCucco,
+        )
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> createItems(){
+    List<DropdownMenuItem<String>> items = new List();
+
+    for(int i = 1; i <= 14; i++) {
+      items.add(new DropdownMenuItem<String>(
+        value: i.toString(),
+        child: Text(i.toString())
+      ));
+    }
+    return items;
+  }
+
+  ValueNotifier<int> _valueNotifier = ValueNotifier<int>(1);
+
+  void _transitionCucco() {
+    
+    _valueNotifier = ValueNotifier<int>(int.parse(_value));
+    
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+        builder: (context) => CuccoApp(int.parse(_value))),
+    );
+  }
 }
---
+++ lib/repository/database_builder.dart
@@ -0,0 +1,3 @@
+class DatabaseBuilder {
+
+}
