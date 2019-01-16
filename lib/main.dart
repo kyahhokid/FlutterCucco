6b0a7d4b5929e074e80351ab65d1ae8ab6bb3a6a
kyahhokid <kyahhokid@gmail.com>
2019-01-16 05:32:57 -0800

    画面遷移できたあああああああああああああああああああああああああああああああああああああああああああああああああああ

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,73 @@ void main() {
     DeviceOrientation.portraitDown
   ]);
 
-  runApp(
-      ScopedModel<CuccoGameMaster>(
-        model: CuccoGameMaster(),
-        child: CuccoApp()
-      )
-  );
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Startup NameGenerator'),
+            ),
+            body: MainBody()
+        ),
+        routes: <String, WidgetBuilder> {
+          '/cucco': (BuildContext context) => new Cucco(),
+        }
+    );
+  }
+}
+
+class MainBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainBodyState();
+}
+
+class MainBodyState extends State<MainBody> {
+
+  String _value = "1";
+  List<DropdownMenuItem<String>> _items;
+
+  @override
+  Widget build(BuildContext context) {
+    _items = createItems();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        ListTile(
+            title: Text("NPCの人数"),
+            trailing: DropdownButton<String>(value: _value, items: _items, onChanged: ((String newValue) {
+              setState(() {
+                _value = newValue;
+              });
+            })), 
+        ),
+        RaisedButton(
+          child: Text("ゲームスタート"),
+          onPressed: _transitionCucco,
+        )
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> createItems(){
+    List<DropdownMenuItem<String>> items = new List();
+
+    for(int i = 1; i <= 14; i++) {
+      items.add(new DropdownMenuItem<String>(
+        value: i.toString(),
+        child: Text(i.toString())
+      ));
+    }
+    return items;
+  }
+
+  void _transitionCucco() {
+    Navigator.pushNamed(context,'/cucco');
+  }
 }
